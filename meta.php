180b63a2521feaf437212c26b763c7bea39f9e81
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:28:02 -0700

    Fixes issues where extended plugins did not get registered items.

--- meta.php
+++ meta.php
@@ -4,13 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
-		require_once( plugin_dir_path( __FILE__ ) . 'lib/abstracts/Meta_Record_Type.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'lib/factories/Meta_Record_Type_Instance.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'lib/loaders/Meta.php' );
-		Underpin\underpin()->loaders()->add( 'meta', [
-			'registry' => 'Underpin_Meta\Loaders\Meta',
-		] );
-	}
+add_action( 'underpin/before_setup', function ( $instance ) {
+	require_once( plugin_dir_path( __FILE__ ) . 'lib/abstracts/Meta_Record_Type.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'lib/factories/Meta_Record_Type_Instance.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'lib/loaders/Meta.php' );
+	$instance->loaders()->add( 'meta', [
+		'registry' => 'Underpin_Meta\Loaders\Meta',
+	] );
 } );
