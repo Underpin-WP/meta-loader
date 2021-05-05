c4495525b81fa10f2bc964a14d2a356c82d92538
Alex Standiford <a@alexstandiford.com>
2021-05-05 15:52:08 -0700

    Corrects underpin autoloader

--- meta.php
+++ meta.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $instance ) {
+add_action( 'underpin/before_setup', function ( $file ) {
 	require_once( plugin_dir_path( __FILE__ ) . 'lib/abstracts/Meta_Record_Type.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'lib/factories/Meta_Record_Type_Instance.php' );
 	require_once( plugin_dir_path( __FILE__ ) . 'lib/loaders/Meta.php' );
-	$instance->loaders()->add( 'meta', [
+	Underpin\underpin()->get( $file )->loaders()->add( 'meta', [
 		'registry' => 'Underpin_Meta\Loaders\Meta',
 	] );
 } );
